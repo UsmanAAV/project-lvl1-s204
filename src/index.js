2293b45d8d900a7f97f08507849369e9f1ef0aed
UsmanAAV <usman.a.a.v@gmail.com>
2017-10-31 19:32:58 +0500

    fixed ./src/index.js following eslint messages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!\n");
+console.log('Welcome to the Brain Games!\n');
 
 const actual = readlineSync.question('May I have your name? ');
 
-console.log("Hello!, " + actual + "!");
+console.log(`Hello!, ${actual}!`);

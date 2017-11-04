9607235cea8b1e5b22e7a0b8c9f4edfd56c74510
UsmanAAV <usman.a.a.v@gmail.com>
2017-11-04 23:25:16 +0500

    made algorithm more understandable

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,27 +1,25 @@
 import { getRandom, runBrainGame } from '..';
 
-const getProgression = (number1, number2) => {
-  const offset = getRandom(10);
+const getProgression = (start, step) => {
+  const num = (start + (step * getRandom(10)));
   let result = '';
 
   for (let i = 0; i < 10; i += 1) {
-    if (i - offset === 0) {
-      result = ` .. ${result}`;
+    if ((start + (step * i)) === num) {
+      result += ` .. `;
     } else {
-      result = ` ${(number1 - (number2 * (i - offset)))} ${result}`;
+      result += ` ${(start + (step * i))} `;
     }
   }
 
-  return result;
+  return [result, String(num)];
 };
 
 const generateQuestionAnswer = () => {
   const num1 = getRandom(100);
   const num2 = getRandom(4) + 1;
-  const question = getProgression(num1, num2);
 
-  const answer = String(num1);
-  return [question, answer];
+  return getProgression(num1, num2);
 };
 
 const runBrainProgression = () => {
